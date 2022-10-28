4cdee7b059422eb3a6b807c2024f0212caad1ac5
Cassemiro <admergcass@gmail.com>
2022-10-28 17:23:50 -0300

    Update

--- variaveis/exemplo-20.php
+++ variaveis/exemplo-20.php
@@ -1,11 +1,12 @@
 <?php
 
-define("BANCO DE DADOS", [
+//Array Constant
+define("BANCO_DE_DADOS", [
 	'127.0.0.1',
 	'root',//usuario do banco
 	'password',//senha do banco
 	'test'//nome do banco ou instancia
 ]);
-
+	print_r(BANCO_DE_DADOS);
 
 ?>
---
+++ variaveis/exemplo-21.php
@@ -0,0 +1,9 @@
+<?php
+
+//Definição de constaste(nome,valor);
+define("SERVIDOR_IP", "127.0.0.1");
+
+echo "SERVIDOR_IP:".SERVIDOR_IP;
+
+
+?>
