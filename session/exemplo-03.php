b4a4127b03edd2ca6c40949ff078cc8fa6bd3cc6
Cassemiro <admergcass@gmail.com>
2022-10-29 15:27:34 -0300

    Update

---
+++ session/exemplo-03.php
@@ -0,0 +1,16 @@
+<?php
+
+$controle = true;
+
+while($controle){
+	string $ramdom = rand('a','z');
+	if ($ramdom === 'ergfvefgtregfve' ){
+		$controle == false;
+		echo "sair: ".$ramdom;
+	}
+	echo $ramdom." ";
+}
+
+
+
+?>
